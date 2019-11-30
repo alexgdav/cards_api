9f30529c042eb59be028429044b32b4723ece790
alexgdav <alexgdavidenko@gmail.com>
2019-11-30 15:15:15 -0500

    Scaffold Cards resource Created Cards resource with question(string) and answer(string) fields with Deck reference foreign key to be added subsequently. Tested existence of Cards via browser (empty array shown)

---
+++ curl-scripts/auth/h-sign-in.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl "https://glacial-eyrie-35831.herokuapp.com/sign-in" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
---
+++ curl-scripts/auth/h-sign-up.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl "https://glacial-eyrie-35831.herokuapp.com/sign-up" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'",
+      "password_confirmation": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
